ca2b62473cdbf398c43f2535ea27911ed78381f5
nalinioliver <noliver@smith.edu>
2023-02-08 22:45:07 -0500

    These are the final documents for my assignment

--- computer.py
+++ computer.py
@@ -1,10 +1,68 @@
+"""
+   Filename: computer.py
+Description: the file that contains the methods/class for looking at the inventory of the shop, as well as editing it (by buying/selling computers within the store).
+     Author: Nalini Oliver
+     Collaborators: CS TA Hours, Prof. Crouser's OneCard Python example, and Prof Crouser's procedural_resale_shop.py code/ main() code. 
+       Date: 8 February 2023
+"""
 class Computer:
+    
+    '''These are the attributes needed to "describe" the computers for this shop. This information will be used later on to refurbish the computer/update the price, and update the operating system.'''
+    description: str = ""
+    processor_type: str = ""
+    hard_drive_capacity: int 
+    memory: int 
+    operating_system: str = ""
+    year_made: int 
+    price: int 
 
-    # What attributes will it need?
+    '''These are the constructors that are used in the program, which defines the attributes we have shown above (as adapted from Prof Crouser's OneCard example).'''
+    def __init__(self, computer_description, computer_processor, computer_hard_drive, computer_memory, operating_system, computer_age, computer_price) -> None:
+        self.description = computer_description
+        self.processor_type = computer_processor
+        self.hard_drive_capacity = computer_hard_drive
+        self.memory = computer_memory
+        self.operating_system = operating_system
+        self.year_made = computer_age
+        self.price = computer_price
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    
+    '''This method updates a computer's operating system, so that no matter what it comes in with, it will automatically be updated to the newest Mac OS, Mac OS Ventura. This is adapted from the program in Prof. Crouser's procedural program'''
+    def updateOS(self, inputOS):
+        newOS = "MacOS Ventura"
+        if newOS is not None:
+            self.operating_system = "MacOS Ventura"
+        else:
+            print("All operating systems updated!")
+    
+    '''This method refurbishes the computer (AKA, changes its price) based on its age. These guidelines/program is adapted from the program in Prof. Crouser's procedural re-sale shop.'''
+    def refurbish(self, yearmade) -> None:   
+        if self.year_made < 2000:
+            self.price = 10
+        elif self.year_made < 2012:
+            self.price = 100
+        elif self.year_made < 2018:
+            self.price = 550
+        else:
+            self.price = 1000
+    
+'''This is an example of what you would type to use the following methods that I have written above. This includes refurbishing the computer, checking its age, etc.'''
+def main():
+    computerA = Computer("Macbook Air 2022", "M1 Processor", 55.7, 22, "Mac OS Mojave", 2021, 200)
+    print(computerA)
+    print("New computer added to inventory, has a description of", computerA.description, "a price of", computerA.price, "and was made in", computerA.year_made)
+    print("The operating system for this computer is", computerA.operating_system)
+    print("Updating the computer OS...")
+    computerA.updateOS(computerA.operating_system)
+    print("The new operating system is", computerA.operating_system)
+    print("The current price of the computer is", computerA.price, "and the current age of the computer is", computerA.year_made)
+    computerA.refurbish(computerA.year_made)
+    print("The new price of the computer based on its age is", computerA.price)
 
-    # What methods will you need?
+
+main() 
+        
+
+    
+    
+    
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,64 @@
+"""
+   Filename: oo_resale_shop.py
+Description: the file that contains the methods/class for looking at the inventory of the shop, as well as editing it (by buying/selling computers within the store).
+     Author: Nalini Oliver
+     Collaborators: CS TA Hours, Prof. Crouser's OneCard Python example, and Prof Crouser's procedural_resale_shop.py code/ main() code. 
+       Date: 8 February 2023
+"""
+from computer import * 
+
 class ResaleShop:
 
-    # What attributes will it need?
+    ''' These are the attributes for the resale shop. The only attribute included is an empty inventory list. '''
+    inventory = []
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
+    
+    '''' The only constructor we have is based on the attribute "inventory". In this case, it is equivalent to an empty list. ''' 
+    def __init__(self) -> None:
+        self.inventory = []
         pass # You'll remove this when you fill out your constructor
 
-    # What methods will you need?
+    
+    ''' This method will buy a new computer, meaning it will add it to the inventory list.'''
+    def buy(self, object1) -> None:
+       self.inventory.append(object1)
+    
+    '''This method will print the inventory list (and therefore reflect changes when we buy/sell computeres, and therefore "add" or "remove" them from the inventory). This function is adapted from Prof. Crouser's inventory print function in the procedural resale shop.''' 
+    def printinventory(self):
+        if self.inventory:
+        # For each item
+            for self.description in self.inventory:
+            # Print its details
+                print(self.description)
+        else:
+            print("No inventory to display.")
+        
+    ''' This method will "sell" and therefore remove computers from the inventory list.''' 
+    def sellcomputer(self, object2) -> None:
+       self.inventory.remove(object2)
+    
+    ''' This is an example of what you would type to use the following methods that I have written above. This includes "naming" the store based on the class name, and using this store to edit the inventory.''' 
+def main():
+    mystore = ResaleShop()
+    print("Welcome to Nalini's computer shop!")
+    computerA = Computer("Macbook Air 2022", "M1 Processor", 55.7, 22, "Mac OS Mojave", 2021, 200)
+    mystore.buy(computerA)
+    computerB = Computer("Macbook Pro 2012", "Intel Processor", 55.7, 22, "Mac OS Puma", 2012, 50)
+    mystore.buy(computerB)
+    print("We just bought two new computers! here is our inventory")
+    mystore.printinventory()
+    mystore.sellcomputer(computerA)
+    print("We just sold one computer. Now, here is our inventory")
+    mystore.printinventory()
+
+main()
+
+
+
+
+
+
+    
+
+
+
